144c1b94201956cefbb8ce108374b653d381d910
sayanel <maximilien.glineur@hotmail.fr>
2015-01-13 10:23:48 +0100

    premier commit du premier main qui ne fait absolulment rien

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+
+//g++ -Wall main.cpp -o main
+
+
+int main(int argc, char **argv)
+{
+	std::cout << "_ESTETIMACGE" << std::endl;
+ 
+
+	// display window
+	cv::namedWindow("image stabilizat
+
+
+	std::cout << "END_" << std::endl;
+	return 0;
+}
