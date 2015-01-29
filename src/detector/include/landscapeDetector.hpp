d4d281785576d4475bd00ac82f4cbcde58e08eab
kouglov <defconunit@free.fr>
2015-01-29 23:01:57 +0100

    push pre soutenance demo

---
+++ src/detector/include/landscapeDetector.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <iostream>
+#include <string>
+#include <math.h>
+
+#include "opencv2/core/core_c.h"
+#include "opencv2/core/core.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/video/video.hpp"
+#include "opencv2/features2d/features2d.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/nonfree/nonfree.hpp"
+#include "opencv2/objdetect/objdetect.hpp"
+
+using namespace std;
+using namespace cv;
+
+
+int detectAndDisplayLandscape (Mat & frame);
